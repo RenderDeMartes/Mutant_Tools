176c4d302d530b635f87f07bdf4c32da78518ed7
Render de Martes <info@renderdemartes.com>
2021-09-04 08:30:29 -0600

    update fixes

---
+++ UI/CustomWidgets/expandableWidget.py
@@ -0,0 +1,117 @@
+import os
+from maya import cmds
+from maya import mel
+import PySide2
+from PySide2 import QtUiTools
+from PySide2 import QtWidgets
+from PySide2.QtWidgets import *
+from PySide2 import QtGui,QtCore
+from shiboken2 import wrapInstance
+
+from maya.app.general.mayaMixin import MayaQWidgetDockableMixin
+import maya.OpenMayaUI as omui
+
+#--------------------------------------------------------------------------------
+PATH = os.path.dirname(__file__)
+FOLDER = PATH.replace('\\UI\\CustomWidgets', '')
+ICONS_FOLDER = FOLDER + '//Icons//'
+
+#------------------------------------------------------------------------------
+
+class expandableWidget(QtWidgets.QDialog):
+
+    def __init__(self, parent=None, title='ExpandableWidget'):
+
+        self.parent = parent
+        self.title = title
+
+        self.create_widget()
+        self.create_connections()
+
+    #-----------------------------------------------------------------------------------------------------------------
+
+    def create_widget(self):
+        #Top Part
+        exp_box = QGroupBox()
+        self.parent.addWidget(exp_box)
+
+        main_layout = QtWidgets.QVBoxLayout()
+        exp_box.setLayout(main_layout)
+
+        h_layout = QtWidgets.QHBoxLayout()
+
+        main_layout.addLayout(h_layout)
+
+        self.open_button = QtWidgets.QPushButton()
+        self.label_button = QtWidgets.QPushButton(self.title)
+
+        self.open_button.setFixedSize(15,15)
+        self.label_button.setStyleSheet("QPushButton{\n    border-style: none;\n    border-width: 0;\n}")
+
+        h_layout.addWidget(self.open_button)
+        h_layout.addWidget(self.label_button)
+
+        separator = self.create_separator()
+        h_layout.addWidget(separator)
+
+        #Botom Part
+        v_layout = QtWidgets.QVBoxLayout()
+        main_layout.addLayout(v_layout)
+
+        self.frame = QtWidgets.QFrame()
+        frame_layout = QtWidgets.QVBoxLayout()
+        self.frame.setLayout(frame_layout)
+        self.frame.setFrameShadow(QFrame.Plain)
+        self.frame.setFrameShape(QFrame.Box)
+        self.frame.setContentsMargins(0, 0, 0, 0)
+
+        v_layout.addWidget(self.frame)
+
+        #test button
+        test_button1 = QtWidgets.QPushButton('TEST')
+        test_button2 = QtWidgets.QPushButton('TEST2')
+
+        frame_layout.addWidget(test_button1)
+        frame_layout.addWidget(test_button2)
+
+        return frame_layout
+
+    #-----------------------------------------------------------------------------------------------------------------
+
+    def create_separator(self):
+        separator = QtWidgets.QLabel()
+        separator.setStyleSheet("border : 5px solid grey; ")
+        separator.setFixedHeight(1)
+
+        return separator
+
+    #-----------------------------------------------------------------------------------------------------------------
+
+    def create_connections(self):
+        self.open_button.clicked.connect(self.toggle_state)
+        self.label_button.clicked.connect(self.toggle_state)
+
+    def toggle_state(self):
+        if self.frame.isVisible():
+            self.frame.hide()
+        else:
+            self.frame.show()
+        self.change_button_icon()
+
+    def collapse(self):
+        self.frame.hide()
+        self.change_button_icon()
+
+
+    def expand(self):
+        self.frame.show()
+        self.change_button_icon()
+
+    def change_button_icon(self):
+        if self.frame.isVisible():
+            self.open_button.setIcon(QtGui.QIcon(ICONS_FOLDER + 'DDown.png'))
+        else:
+            self.open_button.setIcon(QtGui.QIcon(ICONS_FOLDER + 'DRight.png'))
+
+
+
