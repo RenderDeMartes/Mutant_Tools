a37a7c84248c98087371a4955bac48c89c6963eb
Render de Martes <info@renderdemartes.com>
2022-03-12 18:07:07 -0600

    Skull Block

--- Blocks/03_Facial/exec_skull.py
+++ Blocks/03_Facial/exec_skull.py
@@ -79,19 +79,7 @@ def build_skull_block():
     config = cmds.listConnections(block)[1]
     block = block[0]
     guide = cmds.listRelatives(block, c=True)[0]
-
-    #groups for later cleaning
-    clean_rig_grp = ''
-    clean_ctrl_grp = ''
-    
-    #cmds.getAttr('{}.AttrName'.format(config))
-    #cmds.getAttr('{}.AttrName'.format(config), asString = True)
-
-    #orient the joints
-    mt.orient_joint(input = guide)
-    new_guide = mt.duplicate_and_remove_guides(guide)
-    print (new_guide)
-    to_build = [new_guide]
+    name = guide.replace('_Lwr'+nc['guide'], '')
 
     #use this locator in case parent is set to new locator
     if cmds.getAttr('{}.SetParent'.format(config)) == 'new_locator':
@@ -99,9 +87,40 @@ def build_skull_block():
     else:
         block_parent = cmds.getAttr('{}.SetParent'.format(config))
 
+    ctrl_size = cmds.getAttr('{}.CtrlSize'.format(config))
+    ctrl_type = cmds.getAttr('{}.CtrlType'.format(config), asString = True)
+    ctrl_color = cmds.getAttr('{}.CtrlColor'.format(config), asString = True)
 
     #clean a bit
-    clean_rig_grp = cmds.group(em=True, n = '{}{}'.format(block.replace(nc['module'],'_Rig'), nc['group']))
+    clean_ctrl_grp = cmds.group(em=True, name = name + nc['ctrl'] + nc['group'])
+    clean_rig_grp = cmds.group(em=True, name = name + '_Rig' + nc['group'])
+
+    #orient the joints
+    new_guide = mt.duplicate_and_remove_guides(guide)
+    lower_head = cmds.listRelatives(new_guide, c=True)[0]
+    cmds.parent(lower_head,  w=True)
+    for jnt in [new_guide, lower_head]:
+        jnt_root = mt.root_grp(input=jnt)
+        ctrl = mt.curve(input=jnt,
+                              type=ctrl_type,
+                              rename=True,
+                              custom_name=True,
+                              name=jnt.replace(nc['joint'], nc['ctrl']),
+                              size=ctrl_size)
+        mt.assign_color(color=ctrl_color)
+        ctrl_root = mt.root_grp()[0]
+        mt.match(ctrl_root, jnt, r=True, t=True)
+        cmds.connectAttr(ctrl+'.rotate', jnt+'.rotate')
+        cmds.connectAttr(ctrl+'.translate', jnt+'.translate')
+        mt.hide_attr(input=ctrl, s=True)
+        cmds.parent(ctrl_root, clean_ctrl_grp)
+        cmds.parent(jnt_root, clean_rig_grp)
+
+
+    cmds.parent(clean_rig_grp, '{}{}'.format(setup['rig_groups']['misc'], nc['group']))
+    cmds.parent(clean_ctrl_grp, setup['base_groups']['control'] + nc['group'])
+
+    cmds.parentConstraint(block_parent, clean_ctrl_grp)
 
 
     print ('Build {} Success'.format(block))
